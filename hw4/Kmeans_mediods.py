830026b72c04c74b9ba9e3d0511d68cd176689c0
dabingougou <dabingougou@gmail.com>
2021-11-18 01:56:36 +0800

    Trying to code K-medoids but have not yet succeed.

--- hw4/Kmeans_mediods.py
+++ hw4/Kmeans_mediods.py
@@ -1,4 +1,5 @@
 import numpy as np
+# from sklearn_extra.cluster import KMedoids
 K = 2
 
 x = np.array([[0, -6],
@@ -11,12 +12,27 @@ N = x.shape[0]
 
 z = np.array([x[3], x[0]])
 
-x_sub_z = np.empty(shape=(D, N, K))
+x_sub_z = np.empty(shape=(N, K, D))
 for j in range(z.shape[0]):
     for i in range(x.shape[0]):
-        x_sub_z[:, i, j] = x[i] - z[j]
+        x_sub_z[i, j, :] = x[i] - z[j]
 
+l1_norm = np.linalg.norm(x_sub_z, ord=1, axis=2)
+
+# mask = l1_norm==np.max(l1_norm, axis=1)
 #l1_norm = x - z
-print(z)
+print(f'z:\n{z}\n')
 print(f'x vector minus z vector:\n{x_sub_z}\n')
-#print(x_sub_z[2, 0, :])
+print(f'l1 vector:\n{l1_norm}\n')
+# print(f'mask:\n{mask}\n')
+
+#print(np.linalg.norm(x_sub_z[0, 0, :], ord=1, axis=2))
+vec = x_sub_z[2, 0, :]
+print(vec)
+print(l1_norm.argmax(axis=1))
+
+groupings = l1_norm.argmax(axis=1)
+
+for j in range(K):
+    for i in range(N):
+        if groupings[i] = j
