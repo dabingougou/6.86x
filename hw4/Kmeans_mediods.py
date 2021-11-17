e02f56df5b9cab1cfbdde953977b136572c9b229
dabingougou <dabingougou@gmail.com>
2021-11-18 00:02:10 +0800

    Add probabilities 0.5 0.5

--- hw4/Kmeans_mediods.py
+++ hw4/Kmeans_mediods.py
@@ -7,9 +7,16 @@ x = np.array([[0, -6],
               [-5, 2]])
 
 D = x.shape[1]
+N = x.shape[0]
 
 z = np.array([x[3], x[0]])
 
-l1_norm = x - D
+x_sub_z = np.empty(shape=(D, N, K))
+for j in range(z.shape[0]):
+    for i in range(x.shape[0]):
+        x_sub_z[:, i, j] = x[i] - z[j]
+
+#l1_norm = x - z
 print(z)
-print(l1_norm)
+print(f'x vector minus z vector:\n{x_sub_z}\n')
+#print(x_sub_z[2, 0, :])
